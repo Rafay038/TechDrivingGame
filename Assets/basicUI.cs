396c62ffc340c212c8887f1141e93bdaffd543fc
Mirko Musumeci <getmirkomusumeci@gmail.com>
2025-05-03 20:46:06 +0200

    Basic controls and a reset
    - Added controls on screen at the start
    - Added a reset scene option

--- Assets/basicUI.cs
+++ Assets/basicUI.cs
@@ -9,9 +9,21 @@ public class basicUI : MonoBehaviour
     [SerializeField]
     private TMP_Text displayText;
 
+    private bool displayTutorial = true;
+
+    private void Start()
+    {
+        displayText.text = "Controls:\n\n\tPress W or ArrowUp to accelerate\n\n\tPress S or ArrowDown to reverse\n\n\tPress Spacebar to brake\n\n\tPress R to reset";
+    }
+
     // Update is called once per frame
     void Update()
     {
-        displayText.text = "Speed: " + CarController.carSpeed + "km/h";
+        // Stop displaying the tutorial text once the car starts moving
+        if (CarController.carSpeed != 0)
+            displayTutorial = false;
+
+        if (!displayTutorial)
+            displayText.text = "Speed: " + CarController.carSpeed + "km/h";
     }
 }
